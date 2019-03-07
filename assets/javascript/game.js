096ba828168d79e0b399905c2bd179f563edd0a1
Scars and Bullets <sbrough@gmail.com>
2019-03-07 15:28:28 -0700

    game setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,23 @@
-document.
-
 var imgArr = [
 	"../images/baby-unicorn.jpg",
 	"../images/fat-unicorn.jpg",
 	"../images/Hunenberg.png",
-	"../images/Life.png"
+	"../images/Life.jpg"
 ];
 
+var targetNumber = 0;
+var jewels = [];
+var userNumber = 0;
+var wins = 0;
+var losses = 0;
+
+function initialize() {
+	targetNumber = 0;
+	jewels = [];
+	userNumber = 0;
+}
+
+//Random # generator
 function getRand(x, y) {
 	var rand = Math.floor(Math.random() * x + y);
 	console.log(rand);
@@ -16,7 +27,7 @@ function getRand(x, y) {
 //generates a random number between 19-120
 //*12, 0-11
 //*12 + 1, 1-12
-function getTargetRand() {
+function getTarget() {
 	var x = 121 - 19;
 	var y = 19;
 	var rand = getRand(x, y);
@@ -27,10 +38,53 @@ function getTargetRand() {
 //generates a random number between 1-12
 //*12, 0-11
 //*12 + 1, 1-12
-function getJewelRand() {
+function getJewel() {
 	var x = 12;
 	var y = 1;
 	var rand = getRand(x, y);
 	console.log("Jewel " + rand);
 	return rand;
 }
+
+function get4Jewels() {
+	for (var i = 0; i < imgArr.length; i++) {
+		var temp = getJewel();
+		unique = false;
+		while (!unique) {
+			unique = true;
+			if (!jewels.includes(temp)) {
+				jewels[i] = temp;
+				unique = true;
+			} else {
+				temp = getJewel();
+				unique = false;
+			}
+		}
+	}
+}
+
+initialize();
+targetNumber = getTarget();
+get4Jewels();
+
+$("#targetNumber").text(targetNumber);
+$("#userNumber").text(userNumber);
+
+$(document).ready(function() {
+	$("#img1").click(function() {
+		userNumber += jewels[0];
+		alert(userNumber);
+	});
+	$("#img2").click(function() {
+		userNumber += jewels[1];
+		alert(userNumber);
+	});
+	$("#img3").click(function() {
+		userNumber += jewels[2];
+		alert(userNumber);
+	});
+	$("#img4").click(function() {
+		userNumber += jewels[3];
+		alert(userNumber);
+	});
+});
